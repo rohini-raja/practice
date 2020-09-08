e13728c4a5b9257e1ee8d2bc91e661853cc99610
Rohini R <52858735+rohini-raja@users.noreply.github.com>
2020-09-08 22:01:21 +0530

    Add files via upload

---
+++ reverse_a_string.cpp
@@ -0,0 +1,10 @@
+//LEETCODE - STRINGS - ROHINI.R - 8/9/20
+//Reverse a string
+
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        reverse(s.begin(),s.end());
+    }
+};
